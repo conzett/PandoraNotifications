14dc1a060af5071e40929923d2cef7d5ba6feeca
Graham Conzett <conzett@gmail.com>
2011-10-14 20:22:34 -0400

    Change notification checker to use timed polling instead of DOM mutations, fixes album art behavior

--- src/pandora-notification.js
+++ src/pandora-notification.js
@@ -1,38 +1,23 @@
-var check = false;
+var currentArtSRC;
 
-function Delay() {
+function Request(artSRC) {
+	
     _song = document.getElementsByClassName("playerBarSong")[0].textContent;
-    _art = document.getElementsByClassName("playerBarArt")[0].getAttribute("src");
     _artist = document.getElementsByClassName("playerBarArtist")[0].textContent;
     _album = document.getElementsByClassName("playerBarAlbum")[0].textContent;
 
-    chrome.extension.sendRequest({greeting: "song-change", art : _art, artist : _artist, album : _album, song : _song}, function(response) {
-        console.log(response.farewell);
+    chrome.extension.sendRequest({greeting: "song-change", art : artSRC, artist : _artist, album : _album, song : _song}, function(response) {
+        console.log("Pandora Notifications - " + response.farewell);
     });
 }
 
-
-function OnMutation () {
-
-    //Prevent DOMSubtreeModified calling this twice due to text node insertion and removal
-
-    if(check){
-        setTimeout("Delay()", 2000);
-        check = false;
-    }else{
-        check = true;
-    }
+function CheckForChange() {
+	polledArtSRC = document.getElementsByClassName("playerBarArt")[0].getAttribute("src"); // assumes only 1 element of that class on the page
+	if(polledArtSRC != "" && polledArtSRC != currentArtSRC){
+		console.log("Pandora Notifications - Song Change");
+		currentArtSRC = polledArtSRC;
+		Request(polledArtSRC);
+	}
 }
 
-function CheckForPlayerBar() {
-       container = document.getElementsByClassName("playerBarSong")[0]
-       if(container != null){
-           clearInterval ( checkTimer );
-           container.addEventListener ('DOMSubtreeModified', OnMutation, false);
-           console.log("Successfully bound event listener");
-       }else{
-           console.log("Track info not found, checking again in 2 seconds");
-       }
-}
-
-checkTimer = setInterval ( "CheckForPlayerBar()", 2000 );
+checkTimer = setInterval ( "CheckForChange()", 2000 );
