f1fcc2dd4727bb3c057433526548b1b3fa3ed2fb
Graham Conzett <conzett@gmail.com>
2011-11-29 03:29:27 -0500

    Add custom style for notification

--- src/pandora-notification.js
+++ src/pandora-notification.js
@@ -1,5 +1,6 @@
 var currentArtSRC;
 var polledArtSRC;
+var pollInterval = 1000;
 
 function Request(artSRC) {
 	
@@ -9,26 +10,25 @@ function Request(artSRC) {
 
     chrome.extension.sendRequest({greeting: "song-change", art : artSRC, artist : _artist, album : _album, song : _song}, function(response) {
         console.log("Pandora Notifications - " + response.farewell);
+        pollInterval = (response.pollInterval * 1000);
     });
 }
 
 function CheckForChange() {
+
 	var polledArt = document.getElementsByClassName("playerBarArt")[0]; // assumes only 1 element of that class on the page
-		
-	if(polledArt != null){
-		var polledArtSRC = polledArt.getAttribute("src");
-		if(polledArtSRC.charAt(0) === '/'){
-				polledArtSRC = "http://www.pandora.com" + polledArtSRC; //handle images with local paths
-		}
-		if(polledArtSRC != currentArtSRC){
-			console.log("Pandora Notifications - Song Change");
 			
-			currentArtSRC = polledArtSRC;
-			Request(polledArtSRC);
+		if(polledArt != null){
+			var polledArtSRC = polledArt.getAttribute("src");
+
+			if(polledArtSRC.charAt(0) === '/'){
+					polledArtSRC = "http://www.pandora.com" + polledArtSRC; //handle images with local paths
+			}
+			if(polledArtSRC != currentArtSRC){
+					console.log("Pandora Notifications - Song Change");			
+					currentArtSRC = polledArtSRC;
+					Request(polledArtSRC);
+			}
 		}
-	}	
 }
-
-var pollInterval = (localStorage["pollInterval"] * 1000);
-
-checkTimer = setInterval ( "CheckForChange()",  pollInterval);
+setInterval("CheckForChange()", pollInterval);
